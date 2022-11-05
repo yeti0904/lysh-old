2bb53be342871393d38b79cf5d40638029fafe66
yeti0904 <mesyeti@protonmail.com>
2022-11-05 12:11:50 +0000

    make lysh set SHELL

--- source/app.d
+++ source/app.d
@@ -14,10 +14,15 @@ Usage: ysh [-dt/--dump-tokens]
     Prints out all tokens after a command is typed in
 `;
 
+const string appName    = "lysh";
+const string appVersion = "v0.1.0";
+
 void main(string[] args) {
 	bool   run           = true;
 	bool   dumpTokens    = false;
 	string defaultPrompt = environment.get("USER", "") == "root"? "# " : "$ ";
+
+	environment["SHELL"] = args[0];
 	
 	for (size_t i = 1; i < args.length; ++i) {
 		if (args[i][0] == '-') {
@@ -31,6 +36,11 @@ void main(string[] args) {
 				case "-dt":
 				case "--dump-tokens": {
 					dumpTokens = true;
+					break;
+				}
+				case "-v":
+				case "--version": {
+					writefln("%s %s", appName, appVersion);
 					break;
 				}
 				default: {
